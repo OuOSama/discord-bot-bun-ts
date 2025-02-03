b49127c10c26a125cbe01e5413ea9c43274add6b
OuOSama <ouosamacute@example.com>
2025-02-04 01:35:19 +0700

    feat : create events handlers

---
+++ src/handlers/EventHandlers.ts
@@ -0,0 +1,26 @@
+import path from 'path'
+import { loadTsFilesPath } from 'src/lib/loadTsFilesPath'
+import { Client } from 'discord.js'
+
+export default async (client: Client) => {
+  // Get all .ts event files, including those in subfolders
+  const TsEventFiles = await loadTsFilesPath(
+    './{src,build}/Events/**/*.{ts,js}',
+  )
+
+  // Dynamically import event files and register events with the client
+  await Promise.all(
+    TsEventFiles.map(async (file) => {
+      // Import event handler from each file
+      const { default: event } = await import(path.resolve(file))
+
+      // Register the event with the client: 'once' for one-time events, 'on' for repeated events
+      client[event.once ? 'once' : 'on'](event.name, (...args) =>
+        event.execute(...args, client),
+      )
+    }),
+  )
+
+  // Log all event files that were loaded
+  console.log(TsEventFiles)
+}
